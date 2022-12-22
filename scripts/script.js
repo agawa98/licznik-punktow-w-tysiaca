62bb205d636bc7b343974d863cc82c0198b0098c
agawa98 <62704143+agawa98@users.noreply.github.com>
2022-12-22 15:24:23 +0100

    das

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,22 @@
 window.onbeforeunload = function () {return false;}
 
 
+//dostosowywanie szerokosci tabelki z wynikami
+
+window.addEventListener("load", ()=>{
+    document.getElementById("scoreTableDiv").style.width = document.getElementById("tabela").clientWidth-document.getElementById("addScoreTable").clientWidth+"px";
+
+    document.getElementById("newRound").addEventListener("click",newRound)
+    document.getElementById("clear").addEventListener("click",clear)
+
+
+})
+window.addEventListener("resize",()=>{
+    document.getElementById("scoreTableDiv").style.width = document.getElementById("tabela").clientWidth-document.getElementById("addScoreTable").clientWidth+"px";
+})
+
+
+
 function loadscore(){
     if(document.cookie == ""){
         return;
@@ -19,6 +35,14 @@ function loadscore(){
     }
 }
 
+function cookieBuilder(i){
+    document.cookie = "player"+i+"Name="+document.getElementById("player"+i+"Name").value+";expires=Tue, 19 Jan 2038 04:14:07 GMT"
+
+    document.cookie = "player"+i+"Score="+document.getElementById("player"+i+"Total").innerText+";expires=Tue, 19 Jan 2038 04:14:07 GMT"
+}
+
+
+
 function newRound(){
 
     //jesli sa same zera, przerwij
@@ -36,6 +60,7 @@ function newRound(){
         document.getElementById("player"+i+"Total").textContent = Number(document.getElementById("player"+i+"Total").textContent) + Number(document.getElementById("player"+i+"AddScore").value);
         
         let kol = document.createElement('td')
+        kol.className="history"
         kol.textContent = Number(document.getElementById("player"+i+"AddScore").value)
         if(Number(document.getElementById("player"+i+"AddScore").value)==0){
             kol.textContent = "0"
@@ -45,10 +70,26 @@ function newRound(){
         document.getElementById("player"+i+"AddScore").value = null
 
 
-        //cookiebuilder
-        document.cookie = "player"+i+"Name="+document.getElementById("player"+i+"Name").value+";expires=Tue, 19 Jan 2038 04:14:07 GMT"
-
-        document.cookie = "player"+i+"Score="+document.getElementById("player"+i+"Total").innerText+";expires=Tue, 19 Jan 2038 04:14:07 GMT"
+        cookieBuilder(i)
+        
     }
 
+}
+
+
+function clear(){
+    if(confirm("czy jesteś pewien?")==true){
+        for(let i=1;i<5;i++){
+            document.getElementById("player"+i+"Total").textContent = "0"
+            document.getElementById("player"+i+"AddScore").value = null
+            cookieBuilder(i)
+        }
+        historia = document.getElementsByClassName("history")
+        for(let i=0;i<historia.length;i++){
+            historia[i].remove()
+        }
+    }
+    else{
+        return
+    }
 }
